c4abb91b4b42a56399b3e86bcd42c9e95633e2af
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-29 23:56:05 -0500

    Update email to HTML with link to profile

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,11 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: "Welcome to GitHub")
+  end
 
+  def user_connection_email(recipient, sender, subject)
+    @user = recipient
+    mail(to: sender.email, subject: subject)
   end
 
 end
