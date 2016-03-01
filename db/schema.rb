771c7baf31ef9eebc38c3a48efe21c3022986d3a
ndoak <natalieannedoak@gmail.com>
2016-03-01 17:39:54 -0500

    Add Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160301220853) do
-
+ActiveRecord::Schema.define(version: 20160301222741) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -30,7 +28,26 @@ ActiveRecord::Schema.define(version: 20160301220853) do
     t.integer  "skill_level"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "languages", ["user_id"], name: "index_languages_on_user_id"
+
+  create_table "preferences", force: :cascade do |t|
+    t.string   "location"
+    t.boolean  "male"
+    t.boolean  "female"
+    t.boolean  "other"
+    t.integer  "age"
+    t.string   "operating_system"
+    t.boolean  "partner"
+    t.boolean  "paired_programmer"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "user_id"
+  end
+
+  add_index "preferences", ["user_id"], name: "index_preferences_on_user_id"
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -44,7 +61,10 @@ ActiveRecord::Schema.define(version: 20160301220853) do
     t.text     "about_me"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",        null: false
