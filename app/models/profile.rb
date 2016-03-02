f4077216320eebb27ae0463e6c672ef644cc0e4e
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-03-02 14:51:31 -0500

    Change gender validation to include custom error

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,23 @@
+class ValidatesGender < ActiveModel::Validator
+  def validate(profile)
+    unless profile.male.present? || profile.female.present? || profile.other.present?
+      profile.errors[:gender] << "can't be blank"
+    end
+  end
+end
+
 class Profile < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :first_name, :last_name, :location, :birthday, :about_me
+  validates_with ValidatesGender
 
-  validates :male, presence: true, unless: ->(profile){ profile.female.present?|| profile.other.present? }
-  validates :female, presence: true, unless: ->(profile){ profile.male.present? || profile.other.present? }
-  validates :other, presence: true, unless: ->(profile){ profile.male.present? || profile.female.present? }
 
+    #
+    # validates :male, presence: true, unless: ->(profile){ profile.female.present?|| profile.other.present? }
+    # validates :female, presence: true, unless: ->(profile){ profile.male.present? || profile.other.present? }
+    # validates :other, presence: true, unless: ->(profile){ profile.male.present? || profile.female.present? }
+    #
 
 
 end
