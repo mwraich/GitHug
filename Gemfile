bfd930d2fb2d61d0cdca974acba1b71a026d1126
mwraich <mwraich28@gmail.com>
2016-03-02 16:47:50 -0500

    Update profiles schema to include latitude and longitude, geocoder set up

--- Gemfile
+++ Gemfile
@@ -24,8 +24,12 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 #Added Gem
+#for authentications
 gem 'sorcery'
+#to protect API keys
 gem 'figaro'
+#to allow search by location 
+gem 'geocoder'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301222741) do
+ActiveRecord::Schema.define(version: 20160302213744) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20160301222741) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "user_id"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
