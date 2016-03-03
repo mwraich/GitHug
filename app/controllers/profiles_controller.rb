3f332743e8d664b9e47da9485487022905cb24c8
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-03-03 14:07:24 -0500

    fix show method in controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 
   def show
     @user = current_user
-    @profile = Profile.find(@user.profile.id)
+    @profile = Profile.find(params[:id])
   end
 
   def new
