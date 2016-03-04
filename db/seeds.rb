7d4d949fda0820e15391fb53307e2c5250fdba80
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-03-04 17:53:13 -0500

    add styles to robot index view list

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,3 +48,26 @@
 #     paired_programmer: Faker::Boolean.boolean(0.5),
 #   })
 # end
+cities = ["Toronto", "Montreal", "Vancouver", "Calgary"]
+languages = ["Ruby", "Javascript", "Ember.JS", "PHP", "C"]
+100.times do |n|
+ u = User.create!({
+   username: Faker::Internet.domain_word,
+   email: Faker::Internet.email,
+   github_image: Faker::Avatar.image
+ })
+ sleep(1)
+ x = Profile.create!({
+   first_name: Faker::Name.first_name,
+   last_name:  Faker::Name.last_name,
+   location:   cities.sample + ", " + "Canada",
+   male:       true,
+   female:     false,
+   other:      false,
+   birthday:   Faker::Date.backward(5000),
+   operating_system: Faker::App.version,
+   about_me:   Faker::Hacker.say_something_smart,
+   image:      Faker::Avatar.image,
+   user: u
+ })
+end
