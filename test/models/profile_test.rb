a4738397dde6bd05f110fdd9c1d7ce0d79093ab3
ndoak <natalieannedoak@gmail.com>
2016-03-07 15:01:05 -0500

    Try to test Profile model

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -4,4 +4,9 @@ class ProfileTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def test_user_id_is_unique
+    @profile = Profile.new(user_id: 2)
+    assert_not @profile.save,"Profile saved without input"
+  end
 end
