2a6097cc7aa0f5749333a5c2355e1d3812b28df1
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-03-08 20:13:01 -0500

    update color scheme and add javascript to find a match search forms

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -17,30 +17,39 @@ $(document).on('page:load ready', function() {
   $('#date-search').hide();
   $('input[type="radio"]').click(function() {
     if ($(this).attr("value") == "pair") {
-        $("#pair-search").show();
+        $("#search-choice").fadeOut(1000);
+        $("#first_option").fadeOut(1000);
+        $("#pair-search").fadeIn(3000);
         $("#date-search").hide();
+
+
     }
     else {
-        $("#date-search").show();
-          $("#pair-search").hide();
+      $("#search-choice").fadeOut(1000);
+      $("#first_option").fadeOut(1000);
+      $("#date-search").fadeIn(3000);
+      $("#pair-search").hide();
     }
   $('#search-form').on('submit', function(e) {
     e.preventDefault();
-  $.ajax({
-    url: $(this).attr('action'),
-    type: $(this).attr('method'),
-    dataType:'script',
-    data: $(this).serialize(),
-    success: function(data) {
-      if (data) {
-  }
-  else {
-   $("#search-result").html("Sorry, nobody matches your search");
-  }
-}
+    $.ajax({
+      url: $(this).attr('action'),
+      type: $(this).attr('method'),
+      dataType:'script',
+      data: $(this).serialize(),
+      success: function(data) {
+        if (data) {
+          $("#date-search").hide();
+          $("#pair-search").hide();
+        }else{
+          $("#date-search").hide();
+          $("#pair-search").hide();
+          $("#search-result").html("Sorry, nobody matches your search");
+        }
+      }
+    });
   });
 });
-  });
 });
 
 // url: '/profiles?search=' + $('#search').val(),
