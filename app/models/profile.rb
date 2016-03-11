1623a7d3b804155cc394e3c15a652b7f05afcb27
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-03-11 01:45:10 -0500

    Mid commit while working on search

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -17,16 +17,17 @@ class Profile < ActiveRecord::Base
   has_many :blocked_users, through: :users
   has_many :enemies, through: :blocked_users, class_name: User
 
+
   accepts_nested_attributes_for :images, :languages, :preferences, allow_destroy: true
+  before_create :location, :latitude  => :latitude, :longitude => :lon
+  before_update :location, :latitude  => :lat, :longitude => :lon
 
-
-  validates_presence_of :first_name, :last_name, :location, :birthday, :about_me
-  validates_with ValidatesGender
+  validates_presence_of :first_name, :last_name, :city, :province, :birthday, :about_me
+  # validates_with ValidatesGender
   validates :user_id, uniqueness: {message: "Error. Looks like you already have a profile. You can update your profile by clicking on update."}
   geocoded_by :location
-  after_validation :geocode, if: :location_changed?
+  # after_validation :geocode, if: :location_changed?
   acts_as_taggable_on :tags
-
 
   def self.search(search_params)
     m = search_params['male'].to_i.positive?
@@ -42,6 +43,18 @@ class Profile < ActiveRecord::Base
     ).references(:languages)
   end
 
+  def self.pref_search(to_hash)
+    m = ('male').to_i.positive?
+    f = ('female').to_i.positive?
+    o = ('other').to_i.positive?
+    min_age = ('min_age').to_i.years.ago
+    max_age = (('max_age').to_i + 1).years.ago
+
+    where(['location']).includes(:languages).where(male: m, female: f, other: o).where('birthday BETWEEN ? AND ?', max_age, min_age
+    ).where('languages.language = ? OR operating_system like ?', ['language'], ['operating_system']
+    ).references(:languages)
+  end
+
   def self.validate_gender
     unless profile.male.present? || profile.female.present? || profile.other.present?
       profile.errors[:gender] << "can't be blank"
@@ -50,6 +63,23 @@ class Profile < ActiveRecord::Base
 
   def blocked_by?(current_user)
     current_user.enemies.include?(self.user) #Have you blocked this person?
+  end
+
+  # def location
+  #   location = [self.city, self.province].compact.join(', ')
+  # end
+
+  def to_hash
+    {
+    'male' => self.male ? 1 : 0,
+    'female' => self.female ? 1 : 0,
+    'other' => self.other ? 1 : 0,
+    'operating_system' => self.operating_system,
+    'language' => self.pref_languages.first.pref_lang,
+    'min_age' => self.min_age,
+    'max_age' => self.max_age,
+    'location' => self.location
+    }
   end
 
 end
