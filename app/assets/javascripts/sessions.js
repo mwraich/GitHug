00e6c49b9a54a7be214673ca6a8594079c167465
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-03-15 12:51:28 -0400

    modify type animation

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,7 +1,7 @@
 $(document).on('page:load ready', function() {
   $(function(){
       $(".logoTyped").typed({
-        strings: ["GitHub", "GitHug^1000<br>Are you DTC?^1000<br>DTC: Down to code.^1000<br>Login now."],
+        strings: ["GitHub", "GitHug^1000<br>Are you DTC?^1000 <br>DTC: Down to code.^1000<br>Login now."],
         typeSpeed: 100,
         cursorChar: "",
         backDelay: 1000
