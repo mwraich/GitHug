dfea5052111337c4d61f77d32f586abe9cf53eb2
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-03-15 15:22:32 -0400

    Add helper method for show_gender on show page

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,3 +1,19 @@
 module ProfilesHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def recos
+    if profile.partnerReco.any?
+    end
+  end
+
+  def show_gender(profile)
+    if profile.male == true
+      "Male"
+    elsif profile.female == true
+      "Female"
+    else
+      "Other"
+     end
+  end
+  
 end
