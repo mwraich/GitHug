da8e6c5d695dea3ddf7a6c85da64c312aefe28e2
mwraich <mwraich28@gmail.com>
2016-03-16 14:21:48 -0400

    Update form to include email on and off tags

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,14 +5,14 @@ class Message < ActiveRecord::Base
   def send_text_message
   # number_to_send_to = profile.phone
 
-  twilio_sid: ENV["twilio_sid"]
-  twilio_token: ENV["twilio_token"]
-  twilio_phone_number: ENV["twilio_phone_number"]
+  @twilio_sid = ENV["twilio_sid"]
+  @twilio_token = ENV["twilio_token"]
+  @twilio_phone_number = ENV["twilio_phone_number"]
 
-  @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+  @twilio_client = Twilio::REST::Client.new @twilio_sid, @twilio_token
 
   @twilio_client.account.sms.messages.create(
-    :from => "+1#{twilio_phone_number}",
+    :from => "#{twilio_phone_number}",
     :to => profile.phone_number,
     :body => message
   )
