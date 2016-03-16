e9d47ce635d3e2e23b530df5b04ade042e10d36c
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-03-16 19:03:03 -0400

    Update controller to reflect changes to schema

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
 
   def reply
     @message = Message.find(params[:message_id])
-    @reply = Message.new(recipient:@message.sender, subject_line:"RE: #{@message.subject_line}")
+    @reply = Message.new(recipient:@message.sender)
     render partial: "reply"
   end
 
@@ -56,6 +56,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:recipient_id, :message, :subject_line, :read_status)
+    params.require(:message).permit(:recipient_id, :message, :read_status)
   end
 end
