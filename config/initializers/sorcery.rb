2042d1b3247ece74b45750f9759c75af3dda48b3
ndoak <natalieannedoak@gmail.com>
2016-03-21 17:29:34 -0400

    Change callback url for re-deployment

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -5,7 +5,7 @@ Rails.application.config.sorcery.configure do |config|
   config.external_providers = [:github]
   config.github.key = ENV["github_client_id"]
   config.github.secret = ENV["github_secret"]
-  config.github.callback_url = ENV["github_callback_url"]
+  config.github.callback_url = ENV["https://murmuring-dawn-50947.herokuapp.com/oauth/callback?provider=github"]
   config.github.scope = "user" #gives full access to users public page
   config.github.user_info_mapping = {:email => "email", :username => "name", :github_image => "avatar_url"} #:your_data => :providers_data
 
