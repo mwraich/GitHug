5f74475bf9fbda660fdb0cab8932ae068384a644
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-03-21 18:14:18 -0400

    Working on mailer view

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,5 @@
 class Message < ActiveRecord::Base
   belongs_to :recipient, class_name: :Profile
   belongs_to :sender, class_name: :Profile
-  after_create :user_message_notification
-
-  def user_message_notification
-    UserMailer.delay.user_message_notification
-  end
 
 end
