90e2aaaa90e4bbe798ff298ace86bed8d566de7a
mwraich <mwraich28@gmail.com>
2016-03-22 14:52:12 -0400

    update host in config environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_deliveries = true
 
-    config.action_mailer.default_url_options = { :host => 'githug.ca' }
+    config.action_mailer.default_url_options = { :host => 'https://murmuring-dawn-50947.herokuapp.com/' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
