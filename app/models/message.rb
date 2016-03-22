d8bbe9a84c582c3aaa453129b118c0607e9734fc
mwraich <mwraich28@gmail.com>
2016-03-22 13:15:51 -0400

    update config keys in production

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,8 +3,8 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: :Profile
   # after_create :user_message_notification
   #
-  # def user_message_notification
-  #   UserMailer.delay.user_message_notification
-  # end
+  def user_message_notification
+    UserMailer.delay.user_message_notification
+  end
 
 end
