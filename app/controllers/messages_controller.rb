9c890d4458c21ed426db19c661e44a07ada6f71d
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-03-22 13:53:55 -0400

    Update log in link for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,7 +28,7 @@ class MessagesController < ApplicationController
       UserMailer.delay.user_message_notification(Profile.find(@message.recipient), Profile.find(@message.sender))
       redirect_to messages_path, notice: "Message sent!"
     else
-      redirect_to messages_url, alert: "Sorry someething went wrong &   your message could not be send. "
+      redirect_to messages_url, alert: "Sorry someething went wrong & your message could not be send. "
     end
   end
 
