16e74e158f8a8c6d3144cbf054300efb9a222f2d
mwraich <mwraich28@gmail.com>
2016-03-22 14:56:47 -0400

    update port in config environmnets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,11 +17,11 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_deliveries = true
 
-    config.action_mailer.default_url_options = { :host => 'https://murmuring-dawn-50947.herokuapp.com/' }
+    config.action_mailer.default_url_options = { :host => 'githug.ca' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 25,
     domain:               'githug.ca',
     user_name:            ENV["gmail_username"],
     password:             ENV["gmail_password"],
