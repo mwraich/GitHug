562c777b10ee26ff40aaaa569875d47c0d89fc11
Ann Marie Doyle <am.doyle@yahoo.ca>
2017-04-29 18:53:42 -0400

    Updated messages controller to fix minor linting errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,26 @@
+# MessageController handles the messages between users
 class MessagesController < ApplicationController
-
   def index
     @profile = current_user.profile
-    @messages_sent_messages = Message.where(sender: current_user.profile.id).order(:recipient_id, created_at: :desc)
-    @messages_recipient_messages = Message.where(recipient: current_user.profile.id).order(:sender_id, created_at: :desc)
+    @messages_sent_messages = Message.where(sender: current_user.profile.id)
+                                     .order(:recipient_id, created_at: :desc)
+    @messages_recipient_messages = Message.where(recipient: current_user.profile.id)
+                                          .order(:sender_id, created_at: :desc)
     @messages = Message.all
     @received_requests = PullRequest.where(requestee_id: current_user)
   end
 
   def reply
     @message = Message.find(params[:message_id])
-    @reply = Message.new(recipient:@message.sender)
-    render partial: "reply"
+    @reply = Message.new(recipient: @message.sender)
+    render partial: 'reply'
   end
-
 
   def new
     @message = Message.new
   end
 
   def create
-
     @message = Message.new(message_params)
     @message.sender = current_user.profile
     @profile = current_user.profile
@@ -41,19 +41,17 @@ class MessagesController < ApplicationController
       respond_to do |format|
         format.html do
           format.html do
-            render partial:"reply"
+            render partial: 'reply'
           end
-          end
+        end
         format.js
       end
     end
 
     if @message.save && @profile.notification_sms? && @profile.phone_number?
-       Profile.find(@message.recipient).send_text_message(@message)
+      Profile.find(@message.recipient).send_text_message(@message)
     end
-
   end
-
 
   def show
     @profile = Profile.find(params[:id])
@@ -73,9 +71,7 @@ class MessagesController < ApplicationController
     end
   end
 
-
-  def destroy
-  end
+  def destroy; end
 
   private
 
