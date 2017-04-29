e115091fa1b6b3f326f73035a6959b1101f5ed18
Ann Marie Doyle <am.doyle@yahoo.ca>
2017-04-29 18:37:20 -0400

    Linted and cleaned up code in initializers/sorcery

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -3,16 +3,17 @@ Rails.application.config.sorcery.submodules = [:external]
 
 Rails.application.config.sorcery.configure do |config|
   config.external_providers = [:github]
-  config.github.key = ENV["github_client_id"]
-  config.github.secret = ENV["github_secret"]
-  config.github.callback_url = ENV["github_callback_url"]
-  config.github.scope = "user" #gives full access to users public page
-  config.github.user_info_mapping = {:email => "email", :username => "name", :github_image => "avatar_url"} #:your_data => :providers_data
+  config.github.key = ENV['github_client_id']
+  config.github.secret = ENV['github_secret']
+  config.github.callback_url = ENV['github_callback_url']
+  config.github.scope = 'user' # gives full access to users public page
+  config.github.user_info_mapping = {email: 'email', username: 'name', github_image: 'avatar_url'}
+  #:your_data => :providers_data
 
   config.user_config do |user|
     user.username_attribute_names = :first_name
     user.authentications_class = Authentication
   end
 
-  config.user_class = "User"
+  config.user_class = 'User'
 end
